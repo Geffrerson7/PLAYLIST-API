d4ff9a592008edf22f1c0c8ef9cc280a9d8a4939
Gefferson Casasola <gcasasolah@uni.pe>
2023-01-22 15:50:24 -0500

    Actualizacion del login y el README.md

--- README.md
+++ README.md
@@ -1,1 +1,158 @@
 # Proyecto-Unidad-7
+
+REST API with typescript, Express and Prisma.
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`DATABASE_URL`
+
+`PORT`
+
+`JWT_SECRET`
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/Geffrerson7/Proyecto-Unidad-7.git
+```
+
+Go to the project directory
+
+```bash
+  cd Proyecto-Unidad-7
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Install dist directory
+
+```bash
+  npx tsc
+```
+
+Make migrations
+
+```bash
+  npx prisma migrate dev
+```
+
+Run project
+
+```bash
+  npm run dev
+```
+
+## API Reference
+
+### Enpoints description
+
+#### Create user
+
+```http
+  POST /api/v1/users
+```
+
+| Parameter | Type     | Description                |
+| :-------- | :------- | :------------------------- |
+| `name` | `string` | **Required**. User Name|
+| `email` | `string` | **Required**. User email|
+| `password` | `string` | **Required**. User password|
+| `date_born` | `date` | **Required**. User born date |
+
+#### Login
+
+```http
+  POST /api/v1/login
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `email` | `string` | **Required**. User email|
+| `password` | `string` | **Required**. User password|
+
+#### Create song
+
+```http
+  POST /api/v1/songs
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `name` | `string` | **Required**. Song name|
+| `artist` | `string` | **Required**. Artist name|
+| `album` | `string` | **Required**. Album name|
+| `year` | `int` | **Required**. Release year|
+| `genre` | `string` | **Required**. Music genre|
+| `duration` | `int` | **Required**. Playback time in minutes|
+| `isPrivate` | `boolean` | **Required**. Song private status|
+
+#### List songs 
+
+```http
+  GET /api/v1/songs
+```
+
+| Headers | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `Authorization` | `string` | **Required only to see private songs**. Authorization token|
+
+#### Read one song 
+
+```http
+  GET /api/v1/songs/id
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `id` | `int` | **Required**. Song id|
+
+| Headers | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `Authorization` | `string` | **Required only to see private songs**. Authorization token|
+
+#### Create playlist
+
+```http
+  POST /api/v1/playlist
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `name` | `string` | **Required**. Playlist name|
+| `user_id` | `int` | **Required**. User id|
+| `songs` | `list` | **Optional**. List of new songs|
+
+#### Add a song to a playlis
+
+```http
+  PUT /api/v1/playlist/add-song
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `id_song` | `int` | **Required**. Playlist id|
+| `id_playlist` | `int` | **Required**. Song id|
+
+#### List user playlists
+
+```http
+  GET /api/v1/playlist/user/id
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `id` | `int` | **Required**. User id|
+
+## Authors
+
+- [Raisa Orellana](https://github.com/Raisa320)
+- [Jaqueline Ramos](https://github.com/JaquelineRocio)
+- [Gefferson Casasola](https://github.com/Geffrerson7)
--- src/components/user/controller.ts
+++ src/components/user/controller.ts
@@ -1,4 +1,4 @@
-import type {Request, Response} from 'express'
+import type { Request, Response } from 'express'
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
@@ -7,7 +7,7 @@ dotenv.config()
 const prisma = new PrismaClient();
 
 
-export const findAll =async (_req:Request, res: Response): Promise<void> => {
+export const findAll = async (_req: Request, res: Response): Promise<void> => {
 
     try {
         const users = await prisma.user.findMany();
@@ -16,88 +16,97 @@ export const findAll =async (_req:Request, res: Response): Promise<void> => {
             data: users
         });
     } catch (error) {
-        res.status(500).json({ok: false, message: error})
+        res.status(500).json({ ok: false, message: error })
     }
-    
+
 }
-export const store =async (req:Request, res: Response): Promise<void> => {
+export const store = async (req: Request, res: Response): Promise<void> => {
 
     try {
+
         const saltRounds = 10;
         const salt = bcrypt.genSaltSync(saltRounds);
-        const {email, name, password, date_born}= req.body;
-        const user = await prisma.user.create({
-            data: {
-                
-                name,
-                email,
-                password: bcrypt.hashSync(password, salt),
-                date_born: new Date(date_born)
-              }
-        })
-        
-        res.status(201).json({ok: true, message: "Usuario creado correctamente",user})
+        const { email, name, password, date_born } = req.body;
+        let user = await prisma.user.findUnique({
+            where: {
+                email: email,
+            },
+        });
+
+        if (user) {
+            res.status(403).json({ error: "Ese correo ya fue registrado, use otro." });
+        } else {
+            const newUser = await prisma.user.create({
+                data: {
+
+                    name,
+                    email,
+                    password: bcrypt.hashSync(password, salt),
+                    date_born: new Date(date_born)
+                }
+            })
+
+            res.status(201).json({ ok: true, message: "Usuario creado correctamente", newUser })
+        }
+
     } catch (error) {
- 
-        res.status(500).json({ok: false, message: error})
+
+        res.status(500).json({ ok: false, message: error })
     }
 
 
 }
 
-export const login = async (req:Request, res: Response): Promise<void>=>{
+export const login = async (req: Request, res: Response): Promise<void> => {
 
     try {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
 
         let user = await prisma.user.findUnique({
             where: {
-              email: email,
+                email: email,
             },
-          });
+        });
 
-        if(!user) 
-           res.status(403).json({error: "No existe este usuario"});
-        else
-        {
-           const respuestaPassword = await comparePassword(password, user.password); 
-           if(!respuestaPassword)
-            res.status(403).json({error: "Contraseña incorrecta"})
-            else
-            {
+        if (!user) {
+            res.status(403).json({ error: "No existe este usuario" });
+        } else {
+            const respuestaPassword = await comparePassword(password, user.password);
+            if (!respuestaPassword)
+                res.status(403).json({ error: "Contraseña incorrecta" })
+            else {
                 const token = generateToken(user?.id);
                 const loggedUser = await prisma.user.update({
                     where: {
-                      email: user.email,
+                        email: user.email,
                     },
                     data: {
                         last_session: new Date(),
                     },
-                  })
-                res.json({token, login_Date:loggedUser.last_session})
-                
+                })
+                res.json({ token, login_Date: loggedUser.last_session })
+
             }
-            
+
         }
-           
+
     } catch (error) {
         res.status(500).json({ error: "Error de servidor" });
     }
-    
+
 }
 
-const comparePassword = async function(candidatePassword: string, hashPassword: string)
-{   
-    return await bcrypt.compare(candidatePassword,  hashPassword);
+const comparePassword = async function (candidatePassword: string, hashPassword: string) {
+    return await bcrypt.compare(candidatePassword, hashPassword);
 }
 
-export const generateToken = (uid: any) =>{
+export const generateToken = (uid: any) => {
     const expiresIn = "2 days";
     try {
-       const token = jwt.sign({uid}, process.env.JWT_SECRET!, {expiresIn});
+        const token = jwt.sign({ uid }, process.env.JWT_SECRET!, { expiresIn });
 
-       return {token, expiresIn};
-        
+        return { token, expiresIn };
+
     } catch (error) {
         return error;
     }
